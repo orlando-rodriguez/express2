eb15b770ad9a9d29cc2db3ccc606ed47773f9843
Orlando Rodriguez <orlando.r@me.com>
2018-07-10 11:42:59 -0600

    update file structure

---
+++ instructors.js
@@ -0,0 +1,76 @@
+module.exports = {
+    data: [
+        {
+            "id": 1,
+            "full name": "Kyle Coberly",
+            "title": "Faculty Director",
+            "numberOfDogs": 0
+        },
+        {
+            "id": 2,
+            "full name": "Danny Fritz",
+            "title": "Lead Instructor",
+            "numberOfDogs": 0
+        },
+        {
+            "id": 3,
+            "full name": "CJ Reynolds",
+            "title": "Lead Instructor",
+            "numberOfDogs": 0
+        },
+        {
+            "id": 4,
+            "full name": "Brooks Patton",
+            "title": "Lead Instructor",
+            "numberOfDogs": 0
+        },
+        {
+            "id": 5,
+            "full name": "Roberto Ortega",
+            "title": "Lead Instructor",
+            "numberOfDogs": 1
+        },
+        {
+            "id": 6,
+            "full name": "Chad Drummond",
+            "title": "Instructor",
+            "numberOfDogs": 0
+        },
+        {
+            "id": 7,
+            "full name": "Kim Schlesinger",
+            "title": "Instructor",
+            "numberOfDogs": 0
+        },
+        {
+            "id": 8,
+            "full name": "Peter Ostiguy",
+            "title": "Associate Instructor",
+            "numberOfDogs": 1
+        },
+        {
+            "id": 9,
+            "full name": "Cass Torske",
+            "title": "Resident Instructor",
+            "numberOfDogs": 1
+        },
+        {
+            "id": 10,
+            "full name": "Matt Winzer",
+            "title": "Resident Instructor",
+            "numberOfDogs": 2
+        },
+        {
+            "id": 11,
+            "full name": "Aaron Goodman",
+            "title": "Resident Instructor",
+            "numberOfDogs": 2
+        },
+        {
+            "id": 12,
+            "full name": "Michelle Bergquis",
+            "title": "Resident Instructor",
+            "numberOfDogs": 1
+        }
+    ]
+}
